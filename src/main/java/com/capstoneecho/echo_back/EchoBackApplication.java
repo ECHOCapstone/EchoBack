87155eb30b99d97bb16dafe4e387c50e808c9e5d
jiyonggg <kimvergognoso21@hufs.ac.kr>
2026-04-07 11:14:18 +0000

    프로젝트 초기 설정

---
+++ src/main/java/com/capstoneecho/echo_back/EchoBackApplication.java
@@ -0,0 +1,13 @@
+package com.capstoneecho.echo_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchoBackApplication.class, args);
+    }
+
+}
